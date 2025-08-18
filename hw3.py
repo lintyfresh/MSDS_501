d45efbf47f472cab52e8414bbab3c9eb1d19bff3
Lynn Tong <latong@usfca.edu>
2025-08-17 17:10:20 -0700

    added documentation

--- hw3.py
+++ hw3.py
@@ -1,6 +1,11 @@
 # DO NOT ADD ANY OTHER PACKAGES AND LIBRARIES
 
 def create_image_array(file_name):
+    """
+    Create a function called create_image_array() which takes
+    file_name as an input variable and returns a list
+    with the given width and height.
+    """
 
     try: 
         with open(file_name, 'r') as f:
@@ -49,6 +54,13 @@ def create_image_array(file_name):
     return image_array
 
 def xray_filter(numbers): 
+    """
+    Create xray_filter() that takes a list and returns a new list.
+    This new list includes updated r,g,b values that
+    r_value = 255 - r_value
+    g_value = 255 - g_value
+    and b_value = 255 – b_value.
+    """
 
     x_ray_image = list()
 
@@ -65,6 +77,12 @@ def xray_filter(numbers):
 
 
 def adjust_r_g_b(numbers, r_ratio, g_ratio, b_ratio):
+    
+    """
+    Create a function called adjust_r_g_b() that takes the image array and
+    three float values that are multiplied to r,g, and b values accordingly.
+    The adjusted value should be rounded to an integer.
+    """
 
     red_adjustment = r_ratio
     green_adjustment = g_ratio
@@ -87,6 +105,11 @@ def adjust_r_g_b(numbers, r_ratio, g_ratio, b_ratio):
 
 def upside_down(numbers):
 
+    """
+    Create a function called upside_down() that takes a list and
+    reverses the list.
+    """
+
     reversed_array = list()
 
     for row in reversed(numbers):
@@ -95,6 +118,12 @@ def upside_down(numbers):
     return reversed_array
 
 def vertical_flip(numbers):
+
+    """
+    Create a function called vertical_flip() that
+    takes a list and returns a list
+    where values in each row are vertically flipped.
+    """
 
     flipped_array = list()
 
@@ -105,6 +134,20 @@ def vertical_flip(numbers):
     return flipped_array
 
 def create_border(**kwargs):
+    """
+    Add a border around the images with given
+    red, green, blue and pixel values.
+    Input parameters are given as arbitary keyword arguments
+    including numbers, red, green, blue and pixel.
+    "numbers" is a list of pixel values of the input image created
+    by create_image_array().
+    "red", "green", "blue" are indicating values for rgb values.
+    The "pixel" number of [red, green, blue] value should be added
+    at the beginning and end of each row.
+    In addition, the returned list should have
+    the "pixel" number of rows only consists
+    with the given red, green, blue at the beginning and end of "numbers".
+    """
     array3 = kwargs['numbers']
 
     border = kwargs['pixel']
